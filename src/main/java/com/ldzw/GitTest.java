bbc51bc6ef754e8d47c97283171e5510c6a71e6a
zw <zw@qq.com>
2022-11-05 18:01:49 +0800

    first commit

---
+++ src/main/java/com/ldzw/GitTest.java
@@ -0,0 +1,13 @@
+package com.ldzw;
+
+/**
+ * @program: git-test
+ * @description:
+ * @author: zw
+ * @create: 2022-11-05 17:59
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
