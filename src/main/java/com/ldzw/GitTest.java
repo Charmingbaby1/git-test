3cbd23436063487d6e1aa3a214bb146c1b4f083d
zw <zw@qq.com>
2022-11-05 18:12:28 +0800

    hot-fix commit2

--- src/main/java/com/ldzw/GitTest.java
+++ src/main/java/com/ldzw/GitTest.java
@@ -8,10 +8,13 @@ package com.ldzw;
  **/
 public class GitTest {
     public static void main(String[] args) {
+
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("hot-fix test");
+
 
     }
 }
